607828655468cf41339b1bc1c370ab1356d1c552
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-01-03 20:30:09 +0200

    First commit

---
+++ src/test/java/com/tutorhub/TutorhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
