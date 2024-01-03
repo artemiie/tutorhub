36b36ab56d5f3cba6fabe277396f7e58d5e37dc4
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-01-03 21:21:58 +0200

    Add checkstyle

--- src/main/java/com/tutorhub/TutorhubApplication.java
+++ src/main/java/com/tutorhub/TutorhubApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TutorhubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TutorhubApplication.class, args);
-	}
-
+  public static void main(final String[] args) {
+    SpringApplication.run(TutorhubApplication.class, args);
+  }
 }
