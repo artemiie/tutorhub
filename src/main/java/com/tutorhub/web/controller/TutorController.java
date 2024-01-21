7a6a7478bd884c130112f18965a0aeacd7666988
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-01-21 17:46:17 +0200

    Fix checkstyle errors

--- src/main/java/com/tutorhub/web/controller/TutorController.java
+++ src/main/java/com/tutorhub/web/controller/TutorController.java
@@ -11,9 +11,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,7 +41,7 @@ public class TutorController {
   public TutorDTO update(@Validated @RequestBody final TutorDTO tutorDTO) {
     return tutorMapper.toDto(tutorService.update(tutorMapper.fromDto(tutorDTO)));
   }
-  
+
   @GetMapping("/{id}")
   public TutorDTO getById(@PathVariable final ObjectId id) {
     return tutorMapper.toDto(tutorService.getById(id));
