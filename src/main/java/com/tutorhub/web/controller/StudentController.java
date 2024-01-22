a23ba4087ee527ae86e071442be68bf712607fb1
kutas <@Artur-creator-1>
2024-01-22 22:26:06 +0300

    #29 Fix checkstyle errors

--- src/main/java/com/tutorhub/web/controller/StudentController.java
+++ src/main/java/com/tutorhub/web/controller/StudentController.java
@@ -3,7 +3,6 @@ package com.tutorhub.web.controller;
 import com.tutorhub.model.Student;
 import com.tutorhub.service.StudentService;
 import com.tutorhub.web.dto.StudentDTO;
-import com.tutorhub.web.dto.TutorDTO;
 import com.tutorhub.web.dto.mapper.StudentMapper;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
@@ -11,7 +10,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
