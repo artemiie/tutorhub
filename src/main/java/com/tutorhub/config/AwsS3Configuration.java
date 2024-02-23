1c72483791e2c2984f4c2f247fd5637683fdd126
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-02-23 18:44:55 +0200

    Add amazon s3 configuration

---
+++ src/main/java/com/tutorhub/config/AwsS3Configuration.java
@@ -0,0 +1,27 @@
+package com.tutorhub.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.tutorhub.s3.client.AwsS3Properties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class AwsS3Configuration {
+  private final AwsS3Properties awsS3Properties;
+
+  @Bean
+  public AmazonS3 amazonS3() {
+    BasicAWSCredentials credentials =
+        new BasicAWSCredentials(awsS3Properties.getAccessKey(), awsS3Properties.getSecretKey());
+    return AmazonS3ClientBuilder.standard()
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withRegion(Regions.fromName(awsS3Properties.getRegion()))
+        .build();
+  }
+}
