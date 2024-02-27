563772c63129a9b7edd3ed9a62108bde2aef10c5
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-02-27 11:42:48 +0200

    Refactor AwsS3Service.findFile()

--- src/main/java/com/tutorhub/s3/client/service/AwsS3Service.java
+++ src/main/java/com/tutorhub/s3/client/service/AwsS3Service.java
@@ -3,12 +3,7 @@ package com.tutorhub.s3.client.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.util.IOUtils;
-import com.tutorhub.model.course.ContentType;
-import jakarta.annotation.PostConstruct;
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
+import java.io.InputStream;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
@@ -19,30 +14,12 @@ public class AwsS3Service {
   private final String bucket = "tutorhub.storage";
   private final AmazonS3 s3;
 
-  private final Map<ContentType, Function<InputStream, byte[]>> methodByFileType = new HashMap<>();
-
-  @PostConstruct
-  protected void init() {
-    methodByFileType.put(ContentType.IMAGE, this::extractImage);
-    methodByFileType.put(ContentType.VIDEO, this::extractVideo);
-  }
-
   @SneakyThrows
-  public Object findFile(String fileName, ContentType contentType) {
+  public Object find(String fileName) {
     S3Object object = s3.getObject(bucket, fileName);
 
-    var function = methodByFileType.get(contentType);
+    InputStream content = object.getObjectContent();
 
-    return function.apply(object.getObjectContent());
-  }
-
-  @SneakyThrows
-  private byte[] extractImage(InputStream inputStream) {
-    return IOUtils.toByteArray(inputStream);
-  }
-
-  @SneakyThrows
-  private byte[] extractVideo(InputStream inputStream) {
-    return IOUtils.toByteArray(inputStream);
+    return IOUtils.toByteArray(content);
   }
 }
