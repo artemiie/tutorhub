3d592611d6046226f2298a822c2f720f12c5e193
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-02-27 18:07:37 +0200

    Add courseId to module crud operations
    Since a module belongs to a course, we must manage the module based on the course ID and module ID to avoid unauthorized changes

--- src/main/java/com/tutorhub/service/impl/ModuleServiceImpl.java
+++ src/main/java/com/tutorhub/service/impl/ModuleServiceImpl.java
@@ -1,50 +1,56 @@
 package com.tutorhub.service.impl;
 
 import com.tutorhub.exception.ResourceNotFoundException;
+import com.tutorhub.model.course.Course;
 import com.tutorhub.model.course.Module;
 import com.tutorhub.repository.ModuleRepository;
+import com.tutorhub.service.CourseService;
 import com.tutorhub.service.ModuleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ModuleServiceImpl implements ModuleService {
   private final ModuleRepository moduleRepository;
+  private final CourseService courseService;
 
   @Override
-  public Module getById(final Long id) {
+  public Module find(Long courseId, Long moduleId) {
     return moduleRepository
-        .findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("Module with id [" + id + "] not found"));
+        .findByCourseIdAndId(courseId, moduleId)
+        .orElseThrow(
+            () ->
+                new ResourceNotFoundException(
+                    "Module with id = %s not found on Course with id = %s"
+                        .formatted(moduleId, courseId)));
   }
 
   @Override
-  public Page<Module> getAll(final Pageable page) {
-    return moduleRepository.findAll(page);
+  public Page<Module> findAllPaged(Long courseId, Pageable page) {
+    return moduleRepository.findAllByCourseId(courseId, page);
   }
 
   @Override
-  public Module create(final Module entity) {
+  public Module create(Long courseId, final Module entity) {
+    Course course = courseService.getById(courseId);
+    entity.setCourse(course);
     return moduleRepository.save(entity);
   }
 
   @Override
-  public Module update(final Module entity) {
-    Module existingModule = getById(entity.getId());
+  public Module update(Long courseId, final Module entity) {
+    Module existingModule = find(courseId, entity.getId());
     existingModule.setName(entity.getName());
     return moduleRepository.save(existingModule);
   }
 
   @Override
-  public boolean existsById(Long id) {
-    return moduleRepository.existsById(id);
-  }
-
-  @Override
-  public void delete(Long id) {
-    moduleRepository.deleteById(id);
+  @Transactional
+  public void delete(Long courseId, Long moduleId) {
+    moduleRepository.deleteByCourseIdAndModuleId(courseId, moduleId);
   }
 }
