a623170304cfed09a442d68757ce1bfc627b726b
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-02-27 12:08:12 +0200

    Implement upload content to s3

--- src/main/java/com/tutorhub/s3/client/service/AwsS3Service.java
+++ src/main/java/com/tutorhub/s3/client/service/AwsS3Service.java
@@ -1,9 +1,12 @@
 package com.tutorhub.s3.client.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.*;
 import com.amazonaws.util.IOUtils;
+import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
@@ -21,5 +24,53 @@ public class AwsS3Service {
     InputStream content = object.getObjectContent();
 
     return IOUtils.toByteArray(content);
+  }
+
+  public boolean checkIfExists(String filename) {
+    return s3.doesObjectExist(bucket, filename);
+  }
+
+  public void upload(String fileName, File file) {
+
+    long maxPartSize = 5 * 1024 * 1024; // 5 MB
+    long fileLength = file.length();
+
+    if (fileLength <= maxPartSize) {
+      s3.putObject(bucket, fileName, file);
+    } else {
+
+      InitiateMultipartUploadRequest initRequest =
+          new InitiateMultipartUploadRequest(bucket, fileName);
+
+      InitiateMultipartUploadResult initResponse = s3.initiateMultipartUpload(initRequest);
+
+      long uploadedPartsLength = 0;
+      List<PartETag> partETags = new ArrayList<>();
+
+      for (int partNumber = 1; uploadedPartsLength < fileLength; partNumber++) {
+
+        long partSize = Math.min(maxPartSize, (fileLength - uploadedPartsLength));
+
+        UploadPartRequest uploadRequest =
+            new UploadPartRequest()
+                .withBucketName(bucket)
+                .withKey(fileName)
+                .withUploadId(initResponse.getUploadId())
+                .withPartNumber(partNumber)
+                .withPartSize(partSize)
+                .withFileOffset(uploadedPartsLength)
+                .withFile(file);
+
+        UploadPartResult uploadResult = s3.uploadPart(uploadRequest);
+        partETags.add(uploadResult.getPartETag());
+
+        uploadedPartsLength += partSize;
+      }
+      CompleteMultipartUploadRequest compRequest =
+          new CompleteMultipartUploadRequest(
+              bucket, fileName, initResponse.getUploadId(), partETags);
+
+      s3.completeMultipartUpload(compRequest);
+    }
   }
 }
--- src/main/java/com/tutorhub/web/controller/ContentController.java
+++ src/main/java/com/tutorhub/web/controller/ContentController.java
@@ -3,10 +3,8 @@ package com.tutorhub.web.controller;
 import com.tutorhub.service.ContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/content")
@@ -17,5 +15,10 @@ public class ContentController {
   @GetMapping(path = "/{fileName}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
   public Object find(@PathVariable final String fileName) {
     return contentService.find(fileName);
+  }
+
+  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public String upload(@RequestBody MultipartFile file) {
+    return contentService.upload(file);
   }
 }
