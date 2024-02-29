512f6b278612a96944f7b9c9e5a95b1d134eecbc
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-02-29 21:58:10 +0200

    Remove StudentDTO

--- src/main/java/com/tutorhub/web/dto/StudentDTO.java
+++ src/main/java/com/tutorhub/web/dto/StudentDTO.java
@@ -1,16 +1,9 @@
-/*
 package com.tutorhub.web.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class StudentDTO extends UserDTO {
-
-  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-  private List<CourseInfoDTO> coursesInfo;
+public class StudentDTO {
 }
-*/
